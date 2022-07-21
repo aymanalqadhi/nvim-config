06c95d15da4dcd45147724d82a8970806dc911be
Ayman Al-Qadhi <alqd@protonmail.com>
2022-07-21 19:14:03 +0300

    Configure move.nvim keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,4 +1,7 @@
-local opts = { noremap = true, silent = true }
+local opts = {
+    noremap = true,
+    silent = true,
+}
 
 -- disable arrows
 vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', opts)
@@ -17,3 +20,13 @@ vim.api.nvim_set_keymap('n', ';b', [[
 vim.api.nvim_set_keymap('n', '\\\\', '<cmd>Telescope buffers<cr>', opts)
 vim.api.nvim_set_keymap('n', ';;', '<cmd>Telescope help_tags<cr>', opts)
 vim.api.nvim_set_keymap('n', ' .', '<cmd>Telescope lsp_code_actions<cr>', opts)
+
+-- move.nvim
+vim.api.nvim_set_keymap('n', '<A-j>', ":MoveLine(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', ":MoveLine(-1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-j>', ":MoveBlock(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-k>', ":MoveBlock(-1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-l>', ":MoveHChar(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-h>', ":MoveHChar(-1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-l>', ":MoveHBlock(1)<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<A-h>', ":MoveHBlock(-1)<CR>", { noremap = true, silent = true })
