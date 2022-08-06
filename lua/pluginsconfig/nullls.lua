74a94eabe566dcaf209331024fc8282818737571
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-06 23:18:45 +0200

    Configure gitstatus plugin

--- lua/pluginsconfig/nullls.lua
+++ lua/pluginsconfig/nullls.lua
@@ -1,7 +1,11 @@
 local null_ls = require("null-ls")
 
 local function configure()
-  null_ls.setup{}
+  null_ls.setup{
+    sources = {
+      null_ls.builtins.code_actions.gitsigns,
+    }
+  }
 end
 
 return {
