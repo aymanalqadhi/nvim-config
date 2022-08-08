d4e54456c56f2976e363b295004433abee066c73
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-08 13:58:22 +0200

    Disable auto trnasparency mode

--- lua/config.lua
+++ lua/config.lua
@@ -33,25 +33,26 @@ vim.o.visualbell = true
 vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.colorcolumn = "80"
+vim.o.colorcolumn = '80'
 vim.o.conceallevel = 2
 vim.o.lazyredraw = true
 
 -- colorscheme
-vim.g.oceanic_next_terminal_bold = 1
-vim.g.oceanic_next_terminal_italic = 1
+-- vim.g.oceanic_next_terminal_bold = 1
+-- vim.g.oceanic_next_terminal_italic = 1
+vim.cmd 'set termguicolors'
 vim.cmd 'colorscheme onedarkpro'
 
 -- enable transparency
-vim.cmd [[
-  hi Normal guibg=NONE ctermbg=NONE
-  hi LineNr guibg=NONE ctermbg=NONE
-  hi SignColumn guibg=NONE ctermbg=NONE
-  hi EndOfBuffer guibg=NONE ctermbg=NONE
-]]
+--vim.cmd [[
+--  hi Normal guibg=NONE ctermbg=NONE
+--  hi LineNr guibg=NONE ctermbg=NONE
+--  hi SignColumn guibg=NONE ctermbg=NONE
+--  hi EndOfBuffer guibg=NONE ctermbg=NONE
+--]]
 
 --- per-filetype config
 -- *.lua
 vim.api.nvim_command [[
-  autocmd FileType lua setlocal shiftwidth=4 softtabstop=4 expandtab
+  autocmd FileType lua setlocal shiftwidth=2 softtabstop=2 expandtab
 ]]
