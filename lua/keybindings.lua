8d253fd61180f7b21dddeadd34d4abc21f387ea5
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-08 13:58:52 +0200

    Shorten key binding code

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,62 +1,92 @@
 local opts = { noremap = true, silent = true }
+local map = vim.api.nvim_set_keymap
 
 -- disable arrows
-vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', opts)
-vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', opts)
-vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', opts)
-vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', opts)
+map('n', '<Up>', '<Nop>', opts)
+map('n', '<Down>', '<Nop>', opts)
+map('n', '<Left>', '<Nop>', opts)
+map('n', '<Right>', '<Nop>', opts)
 
 -- telescope
-vim.api.nvim_set_keymap('n', ';f', '<cmd>Telescope find_files<cr>', opts)
-vim.api.nvim_set_keymap('n', ';r', [[
+map('n', ';f', '<cmd>Telescope find_files<cr>', opts)
+map('n', ';r', [[
     <cmd>lua require('telescope.builtin').live_grep()<cr>
 ]], opts)
-vim.api.nvim_set_keymap('n', ';b', [[
+map('n', ';b', [[
     <cmd>lua require('telescope.builtin').file_browser()<cr>
 ]], opts)
-vim.api.nvim_set_keymap('n', '\\\\', '<cmd>Telescope buffers<cr>', opts)
-vim.api.nvim_set_keymap('n', ';;', '<cmd>Telescope help_tags<cr>', opts)
---vim.api.nvim_set_keymap('n', ' .', '<cmd>Telescope lsp_code_actions<cr>', opts)
+map('n', '\\\\', '<cmd>Telescope buffers<cr>', opts)
+map('n', ';;', '<cmd>Telescope help_tags<cr>', opts)
+--map('n', ' .', '<cmd>Telescope lsp_code_actions<cr>', opts)
 
 -- move.nvim
-vim.api.nvim_set_keymap('n', '<A-j>', ":MoveLine(1)<CR>", opts)
-vim.api.nvim_set_keymap('n', '<A-k>', ":MoveLine(-1)<CR>", opts)
-vim.api.nvim_set_keymap('v', '<A-j>', ":MoveBlock(1)<CR>", opts)
-vim.api.nvim_set_keymap('v', '<A-k>', ":MoveBlock(-1)<CR>", opts)
-vim.api.nvim_set_keymap('n', '<A-l>', ":MoveHChar(1)<CR>", opts)
-vim.api.nvim_set_keymap('n', '<A-h>', ":MoveHChar(-1)<CR>", opts)
-vim.api.nvim_set_keymap('v', '<A-l>', ":MoveHBlock(1)<CR>", opts)
-vim.api.nvim_set_keymap('v', '<A-h>', ":MoveHBlock(-1)<CR>", opts)
+map('n', '<A-j>', ":MoveLine(1)<CR>", opts)
+map('n', '<A-k>', ":MoveLine(-1)<CR>", opts)
+map('v', '<A-j>', ":MoveBlock(1)<CR>", opts)
+map('v', '<A-k>', ":MoveBlock(-1)<CR>", opts)
+map('n', '<A-l>', ":MoveHChar(1)<CR>", opts)
+map('n', '<A-h>', ":MoveHChar(-1)<CR>", opts)
+map('v', '<A-l>', ":MoveHBlock(1)<CR>", opts)
+map('v', '<A-h>', ":MoveHBlock(-1)<CR>", opts)
 
 -- hover.nvim
 vim.keymap.set('n',  'K', require('hover').hover       , { desc='hover.nvim'         })
 vim.keymap.set('n', 'gK', require('hover').hover_select, { desc='hover.nvim (select)' })
 
 -- lspconfig
-vim.api.nvim_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-vim.api.nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-vim.api.nvim_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-vim.api.nvim_set_keymap('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>.', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
---vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<C-j>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<S-C-j>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-vim.api.nvim_set_keymap("n", '<space>f', '<cmd>lua vim.lsp.buf.format{ async =true }<CR>', opts)
-vim.api.nvim_set_keymap('x', '<space>f', '<cmd>lua vim.lsp.buf.range_formatting({})<CR>', opts)
+map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+map('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+map('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+map('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+map('n', '<space>.', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+--map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+map('n', '<C-j>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+map('n', '<S-C-j>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+map("n", '<space>f', '<cmd>lua vim.lsp.buf.format{ async =true }<CR>', opts)
+map('x', '<space>f', '<cmd>lua vim.lsp.buf.range_formatting({})<CR>', opts)
 
 -- trouble
-vim.api.nvim_set_keymap("n", "<space>t", "<cmd>TroubleToggle<cr>", opts)
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", opts)
-vim.api.nvim_set_keymap("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
-vim.api.nvim_set_keymap("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
-vim.api.nvim_set_keymap("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
-vim.api.nvim_set_keymap("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
-vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>", opts)
+map("n", "<space>t", "<cmd>TroubleToggle<cr>", opts)
+map("n", "<leader>xx", "<cmd>Trouble<cr>", opts)
+map("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", opts)
+map("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", opts)
+map("n", "<leader>xl", "<cmd>Trouble loclist<cr>", opts)
+map("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
+map("n", "gr", "<cmd>Trouble lsp_references<cr>", opts)
+
+-- barbar
+-- Move to previous/next
+map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
