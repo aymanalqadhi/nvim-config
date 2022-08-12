64257bababa2121eaa11366f117bf02d906fd600
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-12 07:45:11 +0200

    Tweak keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -34,8 +34,8 @@ vim.keymap.set('n',  'K', require('hover').hover       , { desc='hover.nvim'    
 vim.keymap.set('n', 'gK', require('hover').hover_select, { desc='hover.nvim (select)' })
 
 -- lspconfig
-map('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-map('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+map('n', 'gd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
 map('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
 map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 map('i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
@@ -45,11 +45,6 @@ map('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_fol
 map('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 map('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 map('n', '<space>.', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
---map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-map('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-map('n', '<C-j>', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-map('n', '<S-C-j>', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-map('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
 map("n", '<space>f', '<cmd>lua vim.lsp.buf.format{ async =true }<CR>', opts)
 map('x', '<space>f', '<cmd>lua vim.lsp.buf.range_formatting({})<CR>', opts)
 
@@ -63,13 +58,13 @@ map("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", opts)
 map("n", "gr", "<cmd>Trouble lsp_references<cr>", opts)
 
 -- barbar
--- Move to previous/next
+  -- Move to previous/next
 map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
+  -- Re-order to previous/next
 map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
 map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
+  -- Goto buffer in position...
 map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
 map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
 map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
@@ -80,16 +75,8 @@ map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
 map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
 map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
 map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
--- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
--- Close buffer
 map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
 map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
--- Sort automatically by...
-map('n', '<Space>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
-map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
-map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
-map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
 -- symbols-outline
 map('n', '<A-s>', '<cmd>SymbolsOutline<CR>', opts)
