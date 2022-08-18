d5cf3196da1df5e20288a3ce8d9fec895028bae0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-18 19:57:51 +0300

    Set null-ls offset_encoding to `utf-8` to fix warning

--- lua/pluginsconfig/nullls.lua
+++ lua/pluginsconfig/nullls.lua
@@ -4,7 +4,10 @@ local function configure()
   null_ls.setup{
     sources = {
       null_ls.builtins.code_actions.gitsigns,
-    }
+    },
+    on_init = function(new_client, _) 
+      new_client.offset_encoding = 'utf-8'
+    end,
   }
 end
 
