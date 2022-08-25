0f53a1fd98835be21815e115b18aa2c7ee5ea1d2
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-25 16:24:19 +0300

    Use the new renamed `window_unfocused_color` key instead of the deprecated `window_unfocussed_color`

--- lua/pluginsconfig/onedarkpro.lua
+++ lua/pluginsconfig/onedarkpro.lua
@@ -23,7 +23,7 @@ local function configure()
         cursorline = true, -- Use cursorline highlighting?
         transparency = true, -- Use a transparent background?
         terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
-        window_unfocussed_color = true, -- When the window is out of focus, change the normal background?
+        window_unfocused_color = true, -- When the window is out of focus, change the normal background?
     }
   }
 
