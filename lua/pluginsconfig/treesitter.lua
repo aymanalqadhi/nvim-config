cd634dfc1d5f1a1d9b6e9c0152d3b3e8a1bb4840
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-20 03:55:54 +0300

    Enable treesitter modules

--- lua/pluginsconfig/treesitter.lua
+++ lua/pluginsconfig/treesitter.lua
@@ -2,12 +2,24 @@ local configs = require('nvim-treesitter.configs')
 
 local function configure()
   configs.setup {
+    auto_install = true,
+
+    autotag = { enable = true },
+    indent = { enable = true },
+    illuminate = { enable = true },
+    incremental_selection = { enable = true },
+
     context_commentstring = {
       enable = true,
       commentary_integration = {
         Commentary = 'gc',
       },
-    }
+    },
+
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
   }
 end
 
