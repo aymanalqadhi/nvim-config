720aade120bc5120510e6ff831161e269d9e2b87
Ayman Al-Qadhi <alqd@protonmail.com>
2022-09-20 03:56:06 +0300

    Set null-ls sources

--- lua/pluginsconfig/nullls.lua
+++ lua/pluginsconfig/nullls.lua
@@ -1,9 +1,40 @@
 local null_ls = require("null-ls")
 
+local code_actions = null_ls.builtins.code_actions
+local diagnostics = null_ls.builtins.diagnostics
+local formatting = null_ls.builtins.formatting
+local hover = null_ls.builtins.hover
+
 local function configure()
-  null_ls.setup{
+  null_ls.setup {
     sources = {
-      null_ls.builtins.code_actions.gitsigns,
+      -- code actions
+      code_actions.eslint_d,
+      code_actions.gitsigns,
+      code_actions.proselint,
+      code_actions.refactoring,
+
+      -- formatters
+      formatting.black,
+      formatting.eslint,
+      formatting.rustfmt,
+      formatting.prettier,
+      formatting.clang_format,
+      formatting.prettierd,
+      formatting.dart_format,
+      formatting.cmake_format,
+
+      -- diagnostics
+      diagnostics.eslint,
+      diagnostics.yamllint,
+      diagnostics.gitlint,
+      diagnostics.flake8,
+      diagnostics.eslint_d,
+      diagnostics.codespell,
+      diagnostics.cmake_lint,
+
+      -- hover
+      hover.printenv,
     },
     on_init = function(new_client, _)
       new_client.offset_encoding = 'utf-16'
