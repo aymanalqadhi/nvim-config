660f389bd82a2e8d8ce460d6ebe047d81095a67e
osamamaqtari <almaktary.osama@gmail.com>
2022-10-05 23:28:56 +0300

    Disable lsp signs

--- lua/pluginsconfig/lspzero.lua
+++ lua/pluginsconfig/lspzero.lua
@@ -4,34 +4,34 @@ local function configure()
   -- virtual text errors
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    virtual_text = {
-      spacing = 4,
-      prefix = '◉',
+      underline = true,
+      virtual_text = {
+        spacing = 4,
+        prefix = '◉',
+      }
     }
-  }
   )
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
   })
 
-  local signs = { Error = "", Warn = "", Info = "כֿ", Hint = "" }
+  -- local signs = { Error = "", Warn = "", Info = "כֿ", Hint = "" }
 
-  for sign, icon in pairs(signs) do
-    vim.fn.sign_define(
-      "DiagnosticSign" .. sign,
-      {
-        text = icon,
-        texthl = "Diagnostic" .. sign,
-        linehl = true,
-        numhl = "Diagnostic" .. sign
-      }
-    )
-  end
+  -- for sign, icon in pairs(signs) do
+  --   vim.fn.sign_define(
+  --     "DiagnosticSign" .. sign,
+  --     {
+  --       text = icon,
+  --       texthl = "Diagnostic" .. sign,
+  --       linehl = true,
+  --       numhl = "Diagnostic" .. sign
+  --     }
+  --   )
+  -- end
 
-  vim.o.updatetime = 250
-  vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+  --vim.o.updatetime = 250
+  --vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
   lsp.preset('lsp-compe')
   lsp.nvim_workspace()
