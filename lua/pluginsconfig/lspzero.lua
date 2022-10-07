6b87f9cadd53ae3d6c38fa2daa3843046a3fd42b
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-07 05:06:40 +0300

    Remove unneeded `lspzero` config

--- lua/pluginsconfig/lspzero.lua
+++ lua/pluginsconfig/lspzero.lua
@@ -12,27 +12,7 @@ local function configure()
   }
   )
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-  })
-
-  local signs = { Error = "", Warn = "", Info = "כֿ", Hint = "" }
-
-  for sign, icon in pairs(signs) do
-    vim.fn.sign_define(
-      "DiagnosticSign" .. sign,
-      {
-        text = icon,
-        texthl = "Diagnostic" .. sign,
-        linehl = true,
-        numhl = "Diagnostic" .. sign
-      }
-    )
-  end
-
-  vim.o.updatetime = 250
   vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-
   lsp.preset('lsp-compe')
   lsp.nvim_workspace()
 
