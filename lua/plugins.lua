22f3b5f6d1351bb7ef3ef304467e81c0ef4bca65
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-07 05:06:13 +0300

    Add `lsp-signature` plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
       { 'hrsh7th/cmp-cmdline' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-nvim-lua' },
+      { 'ray-x/lsp_signature.nvim' },
 
       -- snippets
       { 'L3MON4D3/LuaSnip' },
