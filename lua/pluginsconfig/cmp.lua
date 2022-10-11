6952523e1edc4d44ce61d93908958e11f08aa546
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-11 09:09:44 +0300

    Reconfigure snippets jumping mapping

--- lua/pluginsconfig/cmp.lua
+++ lua/pluginsconfig/cmp.lua
@@ -1,39 +1,12 @@
 local cmp = require("cmp")
 local types = require("cmp.types")
 local str = require("cmp.utils.str")
+local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
-local function t(v)
-  return vim.api.nvim_replace_termcodes(v, true, true, true)
-end
-
-local check_back_space = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s" ~= nil
-end
-
-local function tab(fallback)
-  local luasnip = require "luasnip"
-  if vim.fn.pumvisible() == 1 then
-    vim.fn.feedkeys(t "<C-n>", "n")
-  elseif luasnip.expand_or_jumpable() then
-    vim.fn.feedkeys(t "<Plug>luasnip-expand-or-jump", "")
-  elseif check_back_space() then
-    vim.fn.feedkeys(t "<tab>", "n")
-  else
-    fallback()
-  end
-end
-
-local function shift_tab(fallback)
-  local luasnip = require "luasnip"
-  if vim.fn.pumvisible() == 1 then
-    vim.fn.feedkeys(t "<C-p>", "n")
-  elseif luasnip.jumpable(-1) then
-    vim.fn.feedkeys(t "<Plug>luasnip-jump-prev", "")
-  else
-    fallback()
-  end
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local function configure()
@@ -89,8 +62,26 @@ local function configure()
       end,
     },
     mapping = {
-      ["<Tab>"] = cmp.mapping(tab, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(shift_tab, { "i", "s" }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
@@ -104,7 +95,7 @@ local function configure()
     },
     sources = cmp.config.sources {
       { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
+      { name = 'luasnip' },
       { name = 'calc' },
     },
     experimental = {
