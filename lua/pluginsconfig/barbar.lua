194efcf6b15b951220b48d1bd43520c5a0ce87ef
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-18 23:16:54 +0300

    Tweak barbar config

--- lua/pluginsconfig/barbar.lua
+++ lua/pluginsconfig/barbar.lua
@@ -3,17 +3,17 @@ local barbar = require('bufferline')
 local function configure()
 barbar.setup {
   animation = true,
-  auto_hide = false,
+  auto_hide = true,
 
   tabpages = true,
   closable = true,
   clickable = true,
 
   icons = true,
-  icon_custom_colors = false,
+  icon_custom_colors = true,
   icon_separator_active = '▎',
   icon_separator_inactive = '▎',
-  icon_close_tab = '',
+  icon_close_tab = '✕',
   icon_close_tab_modified = '●',
   icon_pinned = '車',
 
@@ -24,7 +24,7 @@ barbar.setup {
   maximum_length = 30,
   semantic_letters = true,
   letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-  no_name_title = nil,
+  no_name_title = ' -- void -- ',
 }
 end
 
