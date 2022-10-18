f72252fd233f1a3a22ed1b74473af2e2134066d1
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-18 06:43:56 +0300

    Fix dap highlighting

--- lua/pluginsconfig/dap.lua
+++ lua/pluginsconfig/dap.lua
@@ -32,16 +32,20 @@ local function configure_codelldb()
   dap.configurations.rust = dap.configurations.cpp
 
   -- signs
-  vim.highlight.create('DapBreakpoint', { ctermbg = 0, guifg = '#993939', guibg = '#31353f' }, false)
-  vim.highlight.create('DapLogPoint', { ctermbg = 0, guifg = '#61afef', guibg = '#31353f' }, false)
-  vim.highlight.create('DapStopped', { ctermbg = 0, guifg = '#98c379', guibg = '#31353f' }, false)
+  vim.api.nvim_set_hl(0, 'DapBreakpoint', { fg = '#993939', bg = '#31353f' })
+  vim.api.nvim_set_hl(0, 'DapLogPoint', { fg = '#61afef', bg = '#31353f' })
+  vim.api.nvim_set_hl(0, 'DapStopped', { fg = '#98c379', bg = '#31353f' })
 
-  vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint', linehl = '', numhl = 'DapBreakpoint' })
-  vim.fn.sign_define('DapBreakpointCondition', { text = 'ﳁ', texthl = 'DapBreakpoint', linehl = '', numhl = 'DapBreakpoint' })
-  vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'DapBreakpoint', linehl = '', numhl = 'DapBreakpoint' })
-  vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint', linehl = '', numhl = 'DapLogPoint' })
-  vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = '', numhl = 'DapStopped' })
+  local function set_sign(group, icon, hl)
+    vim.fn.sign_define(group, { text = icon, texthl = hl, linehl = '', numhl = hl})
+  end
 
+  set_sign('DapBreakpoint', '', 'DapBreakpoint')
+  set_sign('DapBreakpointCondition', 'ﳁ', 'DapBreakpoint')
+  set_sign('DapBreakpointRejected', '', 'DapBreakpoint')
+  set_sign('DapBreakpointRejected', '', 'DapBreakpoint')
+  set_sign('DapLogPoint', '', 'DapLogPoint')
+  set_sign('DapLogPoint', '', 'DapStopped')
 end
 
 local function configure()
