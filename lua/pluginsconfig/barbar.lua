b9e65b6ab47756e04ffe40b166d3a4d0049db750
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-23 19:30:09 +0300

    Format barbar config

--- lua/pluginsconfig/barbar.lua
+++ lua/pluginsconfig/barbar.lua
@@ -1,31 +1,31 @@
 local barbar = require('bufferline')
 
 local function configure()
-barbar.setup {
-  animation = true,
-  auto_hide = true,
+  barbar.setup {
+    animation = true,
+    auto_hide = true,
 
-  tabpages = true,
-  closable = true,
-  clickable = true,
+    tabpages = true,
+    closable = true,
+    clickable = true,
 
-  icons = true,
-  icon_custom_colors = true,
-  icon_separator_active = '▎',
-  icon_separator_inactive = '▎',
-  icon_close_tab = '✕',
-  icon_close_tab_modified = '●',
-  icon_pinned = '車',
+    icons = true,
+    icon_custom_colors = true,
+    icon_separator_active = '▎',
+    icon_separator_inactive = '▎',
+    icon_close_tab = '✕',
+    icon_close_tab_modified = '●',
+    icon_pinned = '車',
 
-  insert_at_end = false,
-  insert_at_start = false,
+    insert_at_end = false,
+    insert_at_start = false,
 
-  maximum_padding = 1,
-  maximum_length = 30,
-  semantic_letters = true,
-  letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-  no_name_title = ' -- void -- ',
-}
+    maximum_padding = 1,
+    maximum_length = 30,
+    semantic_letters = true,
+    letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+    no_name_title = ' -- void -- ',
+  }
 end
 
 return {
