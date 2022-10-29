1a30e77cb52278a273d1ce5c8bdfbf8bc085f1c8
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-29 03:58:59 +0300

    Fix encoding error

--- lua/pluginsconfig/lspzero.lua
+++ lua/pluginsconfig/lspzero.lua
@@ -4,12 +4,12 @@ local function configure()
   -- virtual text errors
   vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-      underline = true,
-      virtual_text = {
-        spacing = 4,
-        prefix = '◉',
-      }
+    underline = true,
+    virtual_text = {
+      spacing = 4,
+      prefix = '◉',
     }
+  }
   )
 
   vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
@@ -19,6 +19,15 @@ local function configure()
   lsp.on_attach(function(client, _)
     require 'illuminate'.on_attach(client)
   end)
+
+  -- clangd encoding offest fix
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  capabilities.offsetEncoding = { "utf-16" }
+
+  lsp.configure('clangd', {
+    capabilities = capabilities,
+    cmd = { "clangd", '--background-index', '--clang-tidy' }
+  })
 
   lsp.setup()
 end
