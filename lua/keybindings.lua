b32de82d530a5cccb6e11b43c75cb5db2d0f6201
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-29 05:58:37 +0300

    Use default border shape for lazygit

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -104,9 +104,6 @@ _G.lazygit = require('toggleterm.terminal').Terminal:new({
   cmd = 'lazygit',
   dir = 'git_dir',
   direction = 'float',
-  float_opts = {
-    border = 'double',
-  },
   on_open = function(term)
     vim.cmd('startinsert!')
     vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', opts)
