3aef7564b171ba6d5ce51bef54ee5b1a8df55e90
Ayman Al-Qadhi <alqd@protonmail.com>
2022-10-29 06:24:55 +0300

    Remove `cmp-calc`

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,7 +21,6 @@ return require('packer').startup(function(use)
       { 'hrsh7th/cmp-path' },
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-nvim-lua' },
-      { 'hrsh7th/cmp-calc' },
       { 'ray-x/lsp_signature.nvim' },
       { 'andersevenrud/cmp-tmux' },
 
