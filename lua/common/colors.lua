03d520661d7e53928d11f83be5b7b454579fd1da
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-11 23:13:15 +0300

    Move colors to a separate module

---
+++ lua/common/colors.lua
@@ -0,0 +1,17 @@
+local tn_colors = require('tokyonight.colors').setup { style = 'night' }
+
+return {
+  bg = tn_colors.bg_statusline,
+  fg = tn_colors.fg,
+  yellow = tn_colors.yellow,
+  cyan = tn_colors.cyan,
+  darkblue = tn_colors.blue0,
+  green = tn_colors.green,
+  orange = tn_colors.orange,
+  violet = tn_colors.purple,
+  magenta = tn_colors.magenta,
+  blue = tn_colors.blue,
+  red = tn_colors.red,
+  light_bg = tn_colors.bg_highlight,
+  primary_blue = tn_colors.blue5,
+}
--- lua/pluginsconfig/feline.lua
+++ lua/pluginsconfig/feline.lua
@@ -1,24 +1,9 @@
 local feline = require('feline')
 local lsp = require('feline.providers.lsp')
 local vi_mode = require('feline.providers.vi_mode')
-local tn_colors = require('tokyonight.colors').setup { style = 'night' }
+local colors = require('../common/colors')
 
 local function configure()
-  local colors = {
-    bg = tn_colors.bg_statusline,
-    fg = tn_colors.fg,
-    yellow = tn_colors.yellow,
-    cyan = tn_colors.cyan,
-    darkblue = tn_colors.blue0,
-    green = tn_colors.green,
-    orange = tn_colors.orange,
-    violet = tn_colors.purple,
-    magenta = tn_colors.magenta,
-    blue = tn_colors.blue,
-    red = tn_colors.red,
-    light_bg = tn_colors.bg_highlight,
-    primary_blue = tn_colors.blue5,
-  }
 
   local vi_mode_colors = {
     NORMAL = colors.primary_blue,
