39083d27c77c8906c1f537f150c597ac60ab32a8
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-15 06:03:39 +0300

    Add treesitter type objects

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -3,6 +3,11 @@ local M = {}
 -- plugin uri
 M.uri = 'nvim-treesitter/nvim-treesitter'
 
+-- plugin requirements
+M.requirements = {
+  'nvim-treesitter/nvim-treesitter-textobjects'
+}
+
 -- plugin configuration function
 function M.configure()
 
@@ -14,6 +19,41 @@ function M.configure()
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = false,
+    },
+
+    -- advanced text objects
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['af'] = { query = '@function.outer', desc = 'A Function' },
+          ['if'] = { query = '@function.inner', desc = 'In Function' },
+          ['ac'] = { query = '@class.outer', desc = 'A Class' },
+          ['ic'] = { query = '@class.in', desc = 'In Class' },
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = { query = '@function.outer', desc = 'Next Function Start' },
+          [']]'] = { query = '@class.outer', desc = 'Next Class Start' },
+        },
+        goto_next_end = {
+          [']M'] = { query = '@function.outer', desc = 'Next Function End' },
+          [']['] = { query = '@class.outer', desc = 'Next Class End' },
+        },
+        goto_previous_start = {
+          ['[m'] = { query = '@function.outer', desc = 'Previous Function Start' },
+          ['[['] = { query = '@class.outer', desc = 'Previous Class Start' },
+        },
+        goto_previous_end = {
+          ['[M'] = { query = '@function.outer', desc = 'Previous Function End' },
+          ['[]'] = { query = '@class.outer', desc = 'Previous Class End' },
+        },
+      },
     },
   }
 
