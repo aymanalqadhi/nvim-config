585cfa8fe8292f9b463c2891b485b1c98d1332c0
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-17 07:53:32 +0300

    Add a keymap to show references

--- lua/plugins/misc/trouble.lua
+++ lua/plugins/misc/trouble.lua
@@ -29,6 +29,7 @@ function M.keymaps()
       w = { '<cmd>TroubleToggle workspace_diagnostics<cr>', 'Toggle Workspace Trouble' },
       l = { '<cmd>TroubleToggle loclist<cr>', 'Toggle Trouble LocList' },
       q = { '<cmd>TroubleToggle quickfix<cr>', 'Toggle Trouble QuickFix' },
+      r = { '<cmd>TroubleToggle lsp_references<cr>', 'Lsp References' },
     },
     [']t'] = { function() trouble.next(opts) end, 'Next Diagnostic' },
     ['[t'] = { function() trouble.previous(opts) end,'Previous Diagnostic' },
--- lua/plugins/scm/init.lua
+++ lua/plugins/scm/init.lua
@@ -1,3 +1,4 @@
 return {
+  'diffview', -- diff utils
   'gitsigns', -- git utilities
 }
