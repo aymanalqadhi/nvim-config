e4e2775f3f6df98a80658f6ef461aef227ca578a
Ayman Al-Qadhi <alqd@protonmail.com>
2022-12-22 03:25:35 +0300

    Specify a max width for cmp menus

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -20,19 +20,21 @@ M.requirements = {
 
 -- plugin configuration function
 function M.configure()
-  local cmp = require("cmp")
+  local cmp = require('cmp')
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-  local luasnip = require("luasnip")
-  local lspkind = require("lspkind")
+  local luasnip = require('luasnip')
+  local lspkind = require('lspkind')
 
   local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
   end
 
   cmp.setup({
     window = {
-      completion = cmp.config.window.bordered(),
+      completion = cmp.config.window.bordered({
+        winhighlight = 'FloatBorder:LspInfoBorder',
+      }),
       documentation = cmp.config.window.bordered(),
     },
     formatting = {
@@ -41,7 +43,10 @@ function M.configure()
         cmp.ItemField.Abbr,
         cmp.ItemField.Menu,
       },
-      format = lspkind.cmp_format {},
+      format = lspkind.cmp_format({
+        mode = 'symbol_text',
+        maxwidth = 80,
+      })
     },
     snippet = {
       expand = function(args)
@@ -49,7 +54,7 @@ function M.configure()
       end,
     },
     mapping = {
-      ["<Tab>"] = cmp.mapping(function(fallback)
+      ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
         elseif luasnip.expand_or_jumpable() then
@@ -59,8 +64,8 @@ function M.configure()
         else
           fallback()
         end
-      end, { "i", "s" }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
         elseif luasnip.jumpable(-1) then
@@ -68,12 +73,12 @@ function M.configure()
         else
           fallback()
         end
-      end, { "i", "s" }),
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm {
+      end, { 'i', 's' }),
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = false,
       },
