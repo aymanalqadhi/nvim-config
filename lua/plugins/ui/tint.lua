0762826f9d40ae66c59c740b513e742aa1653b5a
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-04 19:14:57 +0300

    Ignore nvimtree from tinting

--- lua/plugins/ui/tint.lua
+++ lua/plugins/ui/tint.lua
@@ -6,15 +6,15 @@ M.uri = 'levouh/tint.nvim'
 -- plugin configuration function
 function M.configure()
   require('tint').setup({
-    tint = -45,
-    saturation = 0.6,
+    tint = -15,
+    saturation = 0.4,
     transforms = require("tint").transforms.SATURATE_TINT,
     tint_background_colors = false,
-    highlight_ignore_patterns = { 'WinSeparator', 'Status.*', },
+    highlight_ignore_patterns = { 'WinSeparator', '*LineNr', 'Status.*', },
     window_ignore_function = function(winid)
       local bufid = vim.api.nvim_win_get_buf(winid)
 
-      return --string.find(vim.api.nvim_buf_get_name(bufid), 'NvimTree') or
+      return string.find(vim.api.nvim_buf_get_name(bufid), 'NvimTree') or
              vim.api.nvim_buf_get_option(bufid, 'buftype') == 'terminal' or
              vim.api.nvim_win_get_config(winid).relative ~= ''
     end
