668ea6a49be165bb12fcc1d48cf3d17643739f6a
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-04 19:14:27 +0300

    Make `barbecue` plugin handle refreshes more efficiently

--- lua/plugins/ui/barbecue.lua
+++ lua/plugins/ui/barbecue.lua
@@ -12,7 +12,24 @@ M.requirements = {
 
 -- plugin configuration function
 function M.configure()
-  require('barbecue').setup {}
+  require('barbecue').setup {
+    create_autocmd = false,
+  }
+
+  vim.api.nvim_create_autocmd({
+    "WinScrolled",
+    "BufWinEnter",
+    "CursorHold",
+    "InsertLeave",
+    "BufWritePost",
+    "TextChanged",
+    "TextChangedI",
+  }, {
+    group = vim.api.nvim_create_augroup("barbecue", {}),
+    callback = function()
+      require("barbecue.ui").update()
+    end,
+  })
 end
 
 return M
