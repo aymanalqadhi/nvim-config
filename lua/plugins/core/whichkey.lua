be7a54c3522bcdf08cc6aa4fb737dfd2b08923db
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-05 20:21:16 +0300

    Adjust `which-key` layout

--- lua/plugins/core/whichkey.lua
+++ lua/plugins/core/whichkey.lua
@@ -21,15 +21,15 @@ function M.configure()
     },
     window = {
       border = "rounded", -- none, single, double, shadow
-      margin = { 0, 1, 1, 1 }, -- extra window margin [top, right, bottom, left]
-      padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
-      winblend = 15
+      margin = { 0, 3, 3, 3 }, -- extra window margin [top, right, bottom, left]
+      padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+      winblend = 50
     },
     layout = {
       height = { min = 4, max = 25 }, -- min and max height of the columns
-      width = { min = 20, max = 50 }, -- min and max width of the columns
-      spacing = 3, -- spacing between columns
-      align = "left", -- align columns left, center or right
+      width = { min = 20, max = 40 }, -- min and max width of the columns
+      spacing = 8, -- spacing between columns
+      align = "center", -- align columns left, center or right
     },
     ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
