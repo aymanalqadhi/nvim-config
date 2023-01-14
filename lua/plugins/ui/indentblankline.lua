1398ce87827afd8c002c00f754cb7745d8f98275
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-14 06:43:16 +0300

    Set a more compatible indent character

--- lua/plugins/ui/indentblankline.lua
+++ lua/plugins/ui/indentblankline.lua
@@ -10,7 +10,7 @@ function M.configure()
   vim.opt.listchars:append("space:⋅")
 
   require("indent_blankline").setup {
-    char = '🭱',
+    char = '┆',
     show_current_context = true,
     show_trailing_blankline_indent = false,
     space_char_blankline = " ",
