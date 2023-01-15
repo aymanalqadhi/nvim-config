fa5919ac0487aad1300f0aed6626500e022c393e
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-15 20:09:22 +0300

    Replace `move.nvim` with simple global keybindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,3 +13,9 @@ map('n', '<C-h>', '<cmd>wincmd h<cr>', opts)
 map('n', '<C-j>', '<cmd>wincmd j<cr>', opts)
 map('n', '<C-k>', '<cmd>wincmd k<cr>', opts)
 map('n', '<C-l>', '<cmd>wincmd l<cr>', opts)
+
+-- move
+vim.api.nvim_set_keymap('v', '<A-j>', ":m +1<cr>gv=gv", opts)
+vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<cr>gv=gv", opts)
+vim.api.nvim_set_keymap('n', '<A-j>', "<cmd>m +1<cr>", opts)
+vim.api.nvim_set_keymap('n', '<A-k>', "<cmd>m -2<cr>", opts)
--- lua/plugins/prod/init.lua
+++ lua/plugins/prod/init.lua
@@ -3,7 +3,6 @@ return {
   'comment',     -- comments helper
   'leap',        -- motion
   'luasnip',     -- snippets
-  'move',        -- movement
   'refactoring', -- rafactoring helpers
   'surround',    -- text-objects surround
 }
