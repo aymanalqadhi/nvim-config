829cb6976249da09ac79026fdb1015108c06f46a
Ayman Al-Qadhi <alqd@protonmail.com>
2023-01-23 18:22:37 +0300

    Tweak indentation marker options

--- lua/plugins/ui/indentblankline.lua
+++ lua/plugins/ui/indentblankline.lua
@@ -6,16 +6,15 @@ M.uri = 'lukas-reineke/indent-blankline.nvim'
 -- plugin configuration function
 function M.configure()
   vim.opt.list = true
-  vim.opt.listchars:append("eol:↴")
-  vim.opt.listchars:append("space: ")
+  vim.opt.listchars:append("eol:⌍")
+  --vim.opt.listchars:append("space:·")
 
   require("indent_blankline").setup {
     char = '¦',
     context_char = '¦',
     show_current_context = true,
     show_trailing_blankline_indent = true,
-    show_current_context_start = true,
-    space_char_blankline = " ",
+    use_treesitter = true,
   }
 
 end
