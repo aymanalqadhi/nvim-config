37e5739bc139816a09ea31677e512f81f53e8c5e
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-06 20:02:20 +0300

    Enable space indent char

--- lua/plugins/ui/indentblankline.lua
+++ lua/plugins/ui/indentblankline.lua
@@ -11,7 +11,7 @@ M.load_on_event = 'ColorScheme'
 function M.configure()
   vim.opt.list = true
   vim.opt.listchars:append("eol:⌍")
-  --vim.opt.listchars:append("space:·")
+  vim.opt.listchars:append("space:·")
 
   require("indent_blankline").setup {
     char = '¦',
