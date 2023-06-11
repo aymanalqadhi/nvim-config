a00a8bfa65b83573473eb1a84477bff376c79b9b
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-11 22:46:37 +0300

    Install `nordic` theme

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -1,11 +1,12 @@
 return {
-  'tokyonight',      -- colorscheme
+  --'tokyonight',    -- colorscheme
+  'nordic',          -- colorscheme
   'dashboard',       -- startup page
   'statuscol',       -- status column
   'bufferline',      -- bar
   'illuminate',      -- highlight current word
   'indentblankline', -- indentation markers
-  'feline',         -- status line
+  'feline',          -- status line
   'noice',           -- ui addons
   'nvimtree',        -- file tree
   'ufo',             -- folding
---
+++ lua/plugins/ui/nordic.lua
@@ -0,0 +1,38 @@
+local M = {}
+
+-- plugin uri
+M.uri = 'AlexvZyl/nordic.nvim'
+
+-- plugin configuration function
+function M.configure()
+  require 'nordic'.setup {
+    -- available: 'nordic', 'onedark'.
+    theme = 'nordic',
+
+    -- theme options
+    bold_keywords = true,
+    italic_comments = true,
+    transparent_bg = false,
+    bright_border = true,
+
+    -- theme variant options
+    nordic = { reduced_blue = true },
+    onedark = { brighter_whites = true },
+
+    -- cursorline options
+    cursorline = {
+      bold = false,
+      theme = 'light',
+      hide_unfocused = true,
+    },
+
+    -- plugin specific options
+    noice = { style = 'classic' },
+    telescope = { style = 'classic' },
+    leap = { dim_backdrop = true },
+  }
+
+  vim.cmd [[ colorscheme nordic ]]
+end
+
+return M
