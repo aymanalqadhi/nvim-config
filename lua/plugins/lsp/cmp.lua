e9d7a134523f02d9b862c941bc5f5a713076bd88
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-11 16:53:44 +0300

    Specify kind icons manually instead of using `lspkind`

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -12,10 +12,10 @@ M.dependencies = {
   'hrsh7th/cmp-nvim-lsp',
   'hrsh7th/cmp-nvim-lua',
   'hrsh7th/cmp-cmdline',
-  'onsails/lspkind.nvim',
   'L3MON4D3/LuaSnip',
   'windwp/nvim-autopairs',
 }
+
 -- plugin options
 M.lazy = true
 M.load_on_event = 'LspAttach'
@@ -25,12 +25,40 @@ function M.configure()
   local cmp = require('cmp')
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
   local luasnip = require('luasnip')
-  local lspkind = require('lspkind')
 
   local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
   end
+
+  local cmp_kinds = {
+    Text = '',
+    Method = '',
+    Function = '',
+    Constructor = '',
+    Field = '',
+    Variable = '',
+    Class = '',
+    Interface = '',
+    Module = '',
+    Property = '',
+    Unit = '',
+    Value = '',
+    Enum = '',
+    Keyword = '',
+    Snippet = '',
+    Color = '',
+    File = '󰈙',
+    Reference = '',
+    Folder = '',
+    EnumMember = '',
+    Constant = '󰏿',
+    Struct = '',
+    Event = '',
+    Operator = '󰆕',
+    TypeParameter = '',
+    Copilot = '',
+  }
 
   cmp.setup({
     window = {
@@ -45,10 +73,12 @@ function M.configure()
         cmp.ItemField.Abbr,
         cmp.ItemField.Menu,
       },
-      format = lspkind.cmp_format({
-        mode = 'symbol_text',
-        maxwidth = 80,
-      })
+      format = function(_, vim_item)
+        vim_item.menu = '     · ' .. (vim_item.kind or '')
+        vim_item.kind = (cmp_kinds[vim_item.kind] or vim_item.kind) .. ' '
+
+        return vim_item
+      end,
     },
     snippet = {
       expand = function(args)
