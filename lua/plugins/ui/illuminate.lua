dfa1d1d9f2192c127d9c2c077658044e7d2cae9d
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-12 02:20:25 +0300

    Add configuration template

--- lua/plugins/ui/illuminate.lua
+++ lua/plugins/ui/illuminate.lua
@@ -9,7 +9,21 @@ M.load_on_event = 'LspAttach'
 
 -- plugin configuration function
 function M.configure()
-  require('illuminate').configure {}
+  require('illuminate').configure({
+    delay = 200,
+
+    filetypes_denylist = { 'NvimTree' },
+    filetypes_allowlist = {},
+
+    modes_denylist = {},
+    modes_allowlist = {},
+
+    providers_regex_syntax_denylist = {},
+    providers_regex_syntax_allowlist = {},
+
+    under_cursor = true,
+    min_count_to_highlight = 1,
+  })
 end
 
 return M
