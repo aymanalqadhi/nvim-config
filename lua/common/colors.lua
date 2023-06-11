ec8e6863ba709b778500f4ef6fb5bf76ea75ba20
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-11 23:05:37 +0300

    Make colors work with `nordic` theme

--- lua/common/colors.lua
+++ lua/common/colors.lua
@@ -1,25 +1,55 @@
 local M = {}
 
-local tn_colors = require('tokyonight.colors').setup { style = 'night' }
+local use_theme = 'nordic'
 
--- colors palette
-M.none = tn_colors.none
-M.bg = tn_colors.bg
-M.bg_dark = tn_colors.bg_dark
-M.bg_highlight = tn_colors.bg_highlight
-M.fg = tn_colors.fg
-M.fg_dark = tn_colors.fg_dark
-M.yellow = tn_colors.yellow
-M.cyan = tn_colors.cyan
-M.green = tn_colors.green
-M.orange = tn_colors.orange
-M.violet = tn_colors.purple
-M.magenta = tn_colors.magenta
-M.blue = tn_colors.blue
-M.darkblue = tn_colors.blue0
-M.red = tn_colors.red
-M.light_bg = tn_colors.bg_highlight
-M.primary_blue = tn_colors.blue5
+if use_theme == 'nordic' then
+  local colors = require('nordic.colors')
+
+  M.none = colors.none
+
+  -- background
+  M.bg = colors.black
+  M.bg_dark = colors.gray2
+  M.bg_highlight = colors.gray4
+
+  -- foreground
+  M.fg = colors.white2
+  M.fg_dark = colors.white0
+
+  -- base colors
+  M.red = colors.red.base
+  M.green = colors.green.base
+  M.blue = colors.blue2
+  M.darkblue = colors.blue.dim
+  M.primary_blue = colors.blue.bright
+
+  -- extra colors
+  M.yellow = colors.yellow.base
+  M.cyan = colors.cyan.base
+  M.orange = colors.orange.base
+  M.violet = colors.magenta.dim
+  M.magenta = colors.magenta.base
+
+elseif use_theme == 'tokyonight' then
+  local tn_colors = require('tokyonight.colors').setup { style = 'night' }
+
+  M.none = tn_colors.none
+  M.bg = tn_colors.bg
+  M.bg_dark = tn_colors.bg_dark
+  M.bg_highlight = tn_colors.bg_highlight
+  M.fg = tn_colors.fg
+  M.fg_dark = tn_colors.fg_dark
+  M.yellow = tn_colors.yellow
+  M.cyan = tn_colors.cyan
+  M.green = tn_colors.green
+  M.orange = tn_colors.orange
+  M.violet = tn_colors.purple
+  M.magenta = tn_colors.magenta
+  M.blue = tn_colors.blue
+  M.darkblue = tn_colors.blue0
+  M.red = tn_colors.red
+  M.primary_blue = tn_colors.blue5
+end
 
 -- mode colors
 M.mode_colors = {
