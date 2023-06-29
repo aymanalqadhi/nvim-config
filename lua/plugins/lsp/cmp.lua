bf48715a0ff4fc3dcd8d4833e6cc3f8b6f34b279
Ayman Al-Qadhi <alqd@protonmail.com>
2023-06-29 06:16:56 +0300

    Use icons from `common.icons` module

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -26,41 +26,12 @@ function M.configure()
   local cmp = require('cmp')
   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
   local luasnip = require('luasnip')
+  local icons = require('common.icons')
 
   local has_words_before = function()
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
   end
-
-  local cmp_kinds = {
-    Text = '󰀬',
-    Method = '',
-    Function = '󰊕',
-    Constructor = '󰈏',
-    Field = '',
-    Variable = '󰝄',
-    Class = '',
-    Interface = '',
-    Module = '',
-    Property = '',
-    Unit = '',
-    Value = '',
-    Enum = '',
-    Keyword = '',
-    Snippet = '',
-    Color = '',
-    File = '',
-    Reference = '',
-    Folder = '',
-    EnumMember = '󰉺',
-    Constant = '󰏿',
-    Struct = '',
-    Event = '',
-    Operator = '',
-    TypeParameter = '',
-    Copilot = '',
-    Default = ''
-  }
 
   cmp.setup({
     window = {
@@ -77,7 +48,7 @@ function M.configure()
       },
       format = function(_, vim_item)
         vim_item.menu = '     · ' .. (vim_item.kind or '')
-        vim_item.kind = (cmp_kinds[vim_item.kind] or vim_item.kind) .. ' '
+        vim_item.kind = (icons.lsp_kinds[vim_item.kind] or vim_item.kind) .. ''
         --vim_item.kind = (cmp_kinds[vim_item.kind] or vim_item.kind) .. ' '
 
         return vim_item
