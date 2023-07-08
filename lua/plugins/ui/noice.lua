b67ed1bbfcaea5ca301f4adbd230121edb85a11e
Ayman Al-Qadhi <alqd@protonmail.com>
2023-07-09 02:41:21 +0300

    Tweak window options

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -15,12 +15,12 @@ M.load_on_event = 'LspAttach'
 
 -- plugin configuration function
 function M.configure()
-  require("noice").setup({
+  require('noice').setup({
     lsp = {
       override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true,
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+        ['cmp.entry.get_documentation'] = true,
       },
     },
     presets = {
@@ -29,6 +29,39 @@ function M.configure()
       long_message_to_split = true,
       inc_rename = true,
       lsp_doc_border = true,
+    },
+    views = {
+      cmdline_popup = {
+        border = {
+          style = {
+            top_left = '',
+            top = '',
+            top_right = '',
+            right = '',
+            bottom_right = '',
+            bottom = '',
+            bottom_left = '',
+            left = { '│', 'NoiceFloatBorder' },
+          },
+          padding = { 1, 2 },
+        },
+        win_options = {
+          winhighlight = {
+            Normal = 'NoiceNormal',
+            NormalFloat = 'NoiceFloatNormal',
+          },
+        },
+      },
+    },
+    routes = {
+      {
+        filter = {
+          event = "msg_show",
+          kind = "",
+          find = "written",
+        },
+        opts = { skip = true },
+      },
     },
   })
 end
