4a0b80f4e8b9e458a745a3cb79e469688638c37e
Ayman Al-Qadhi <alqd@protonmail.com>
2023-08-16 22:06:06 +0300

    Update `treesitter-context-objects` config

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -11,57 +11,109 @@ M.dependencies = {
 
 -- plugin configuration function
 function M.configure()
+  local enable = true
+
   require('nvim-treesitter.configs').setup {
     -- auto install missing parsers
     auto_install = true,
 
     -- highlighting module
     highlight = {
-      enable = true,
+      enable = enable,
       additional_vim_regex_highlighting = false,
     },
 
-    -- advanced text objects
+    incremental_selection = {
+      enable = enable,
+      keymaps = {
+        -- mappings for incremental selection (visual mappings)
+        init_selection = "gnn",    -- maps in normal mode to init the node/scope selection
+        node_incremental = "grn",  -- increment to the upper named parent
+        scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
+        node_decremental = "grm"   -- decrement to the previous node
+      }
+    },
+
     textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['af'] = { query = '@function.outer', 'A Function' },
-          ['if'] = { query = '@function.inner', 'In Function' },
-          ['ac'] = { query = '@class.outer', 'A Class' },
-          ['ic'] = { query = '@class.in', 'In Class' },
+      -- syntax-aware textobjects
+      enable = enable,
+      lsp_interop = {
+        enable = enable,
+        peek_definition_code = {
+          ["DF"] = "@function.outer",
+          ["DF"] = "@class.outer"
+        }
+      },
+      keymaps = {
+        ["iL"] = {
+          -- you can define your own textobjects directly here
+          go = "(function_definition) @function",
         },
-        selection_modes = {
-          ['@parameter.outer'] = 'v',
-          ['@function.outer'] = 'V',
-          ['@class.outer'] = '<c-v>',
-        },
-        include_surrounding_whitespace = true,
+        -- or you use the queries from supported languages with textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["aC"] = "@class.outer",
+        ["iC"] = "@class.inner",
+        ["ac"] = "@conditional.outer",
+        ["ic"] = "@conditional.inner",
+        ["ae"] = "@block.outer",
+        ["ie"] = "@block.inner",
+        ["al"] = "@loop.outer",
+        ["il"] = "@loop.inner",
+        ["is"] = "@statement.inner",
+        ["as"] = "@statement.outer",
+        ["ad"] = "@comment.outer",
+        ["am"] = "@call.outer",
+        ["im"] = "@call.inner"
       },
       move = {
-        enable = true,
-        set_jumps = true,
+        enable = enable,
+        set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
           ["]m"] = "@function.outer",
-          ["]]"] = { query = "@class.outer", desc = "Next class start" },
-          ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-          ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+          ["]]"] = "@class.outer"
         },
         goto_next_end = {
           ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
+          ["]["] = "@class.outer"
         },
         goto_previous_start = {
           ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
+          ["[["] = "@class.outer"
         },
         goto_previous_end = {
           ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
+          ["[]"] = "@class.outer"
+        }
       },
-    },
+      select = {
+        enable = enable,
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          -- Or you can define your own textobjects like this
+          ["iF"] = {
+            python = "(function_definition) @function",
+            cpp = "(function_definition) @function",
+            c = "(function_definition) @function",
+            java = "(method_declaration) @function",
+            go = "(method_declaration) @function"
+          }
+        }
+      },
+      swap = {
+        enable = enable,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner"
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner"
+        }
+      }
+    }
   }
 
   require 'treesitter-context'.setup {}
