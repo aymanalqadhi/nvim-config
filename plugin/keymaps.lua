098511feea54466e28efe31eaac75bb7bfa7e65b
Ayman Al-Qadhi <alqd@protonmail.com>
2024-06-11 04:48:19 +0300

    Add window management keymaps

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -1,31 +1,39 @@
 local set = vim.keymap.set
 
 -- window navigation
-set("n", "<c-h>", "<c-w><c-h>", { desc = "left window" })
-set("n", "<c-j>", "<c-w><c-j>", { desc = "lower window" })
-set("n", "<c-k>", "<c-w><c-k>", { desc = "upper window" })
-set("n", "<c-l>", "<c-w><c-l>", { desc = "right window" })
+set("n", "<c-h>", "<c-w><c-h>", { desc = "win: left" })
+set("n", "<c-j>", "<c-w><c-j>", { desc = "win: down" })
+set("n", "<c-k>", "<c-w><c-k>", { desc = "win: up" })
+set("n", "<c-l>", "<c-w><c-l>", { desc = "win: right" })
 
 -- window resizing
-set("n", "<c-left>", "<cmd>vert resize 1<cr>", { desc = "shrink window" })
-set("n", "<c-down>", "<cmd>resize -2<cr>", { desc = "shorten window" })
-set("n", "<c-up>", "<cmd>resize +3<cr>", { desc = "longen window" })
-set("n", "<c-right>", "<cmd>vert resize +2<cr>", { desc = "widen window" })
+set("n", "<c-left>", "<cmd>vert resize 1<cr>", { desc = "win: narrower" })
+set("n", "<c-down>", "<cmd>resize -2<cr>", { desc = "win: shorter" })
+set("n", "<c-up>", "<cmd>resize +3<cr>", { desc = "win: taller" })
+set("n", "<c-right>", "<cmd>vert resize +2<cr>", { desc = "win: wider" })
+
+-- window management
+set("n", "<leader>ws", "<cmd>split<cr>", { desc = "win: split below" })
+set("n", "<leader>wv", "<cmd>vsplit<cr>", { desc = "win: split right" })
+set("n", "<leader>wq", "<cmd>quit<cr>", { desc = "win: quit" })
+set("n", "<leader>wc", "<cmd>close<cr>", { desc = "win: close" })
+set("n", "<leader>wo", "<cmd>only<cr>", { desc = "win: only" })
+set("n", "<leader>we", "<cmd>wincmd =<cr>", { desc = "win: equalize" })
 
 -- buffer navigation
-set("n", "[b", "<cmd>bnext<cr>", { desc = "prev buffer" })
-set("n", "]b", "<cmd>bprevious<cr>", { desc = "next buffer" })
+set("n", "[b", "<cmd>bprevious<cr>", { desc = "buf: prev" })
+set("n", "]b", "<cmd>bnext<cr>", { desc = "buf: next" })
 
 -- buffer managemenet
-set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "prev buffer" })
-set("n", "<leader>ba", "<cmd>ball<cr>", { desc = "prev buffer" })
+set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "buf: delete" })
+set("n", "<leader>ba", "<cmd>ball<cr>", { desc = "buf: all" })
 
 -- clear `hlsearch` with `esc`
 set({ "n", "i" }, "<esc>", "<cmd>nohlsearch<cr><esc>", { desc = "clear `hlsearch`" })
 
 -- better visual-mode indentation
-set("v", "<", "<gv", { desc = "indent left" })
-set("v", ">", ">gv", { desc = "indent right" })
+set("v", "<", "<gv", { desc = "indent: left" })
+set("v", ">", ">gv", { desc = "indent: right" })
 
 -- better command mode
 set("c", "<c-a>", "<home>")
@@ -56,4 +64,5 @@ function _G.set_terminal_keymaps()
   set("t", "<c-k>", "<cmd>wincmd k<cr>", opts)
   set("t", "<c-l>", "<cmd>wincmd l<cr>", opts)
 end
+
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
