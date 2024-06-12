86aeeb3a72d24f76ea2dcc2b56f83ccb36526c13
Ayman Al-Qadhi <alqd@protonmail.com>
2024-06-12 07:19:42 +0300

    Use correct buffer field

--- lua/plugins/editor/lsp.lua
+++ lua/plugins/editor/lsp.lua
@@ -221,7 +221,7 @@ return {
     -- on-attach config
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
-        local bufnr = args.bufnr
+        local bufnr = args.buf
         local client = vim.lsp.get_client_by_id(args.data.client_id)
 
         -- completion
