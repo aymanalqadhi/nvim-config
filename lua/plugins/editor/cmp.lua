0c20f900bf7653bdbe8bffce8ac2befacfec2d55
Ayman Al-Qadhi <alqd@protonmail.com>
2024-06-12 07:19:19 +0300

    Rewrite snippets config

--- lua/plugins/editor/cmp.lua
+++ lua/plugins/editor/cmp.lua
@@ -1,7 +1,9 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+
     branch = "main",
+    event = "VeryLazy",
 
     dependencies = {
       "hrsh7th/cmp-nvim-lsp",
@@ -11,49 +13,62 @@ return {
       "windwp/nvim-autopairs",
     },
 
-    event = { "InsertEnter", "CmdlineEnter" },
-
-    opts = function(_, opts)
+    opts = function()
       local cmp = require("cmp")
-      local icons = require("void.config.icons")
 
-      return vim.tbl_deep_extend("keep", {
+      local icons = require("void.config.icons")
+      local source_names = {
+        buffer   = "Buffer",
+        cmdline  = "Command",
+        copilot  = "Copilot",
+        crates   = "Crates",
+        lazydev  = "LazyDev",
+        luasnip  = "LuaSnip",
+        nvim_lsp = "LSP",
+        path     = "Path",
+      }
+
+      return {
         -- disable tab
         ["<tab>"] = cmp.config.disable,
         ["<s-tab>"] = cmp.config.disable,
 
         -- completion sources
         sources = {
+          { name = "lazydev",  group_index = 0 },
+          { name = "crates",   group_index = 1 },
+          { name = "copilot",  group_index = 2 },
           { name = "nvim_lsp", group_index = 2 },
+          { name = "luasnip",  group_index = 2 },
           { name = "path",     group_index = 4 },
           { name = "buffer",   group_index = 4 },
         },
 
         -- sorting
-        sorting = {
-          priority_weight = 2,
-          comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.locality,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
-        },
+        -- sorting = {
+        --   priority_weight = 2,
+        --   comparators = {
+        --     cmp.config.compare.offset,
+        --     cmp.config.compare.exact,
+        --     cmp.config.compare.score,
+        --     cmp.config.compare.recently_used,
+        --     cmp.config.compare.locality,
+        --     cmp.config.compare.kind,
+        --     cmp.config.compare.sort_text,
+        --     cmp.config.compare.length,
+        --     cmp.config.compare.order,
+        --   },
+        -- },
 
         -- key mappings
         mapping = {
           -- docs
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<c-f>"] = cmp.mapping.scroll_docs(4),
+          ["<c-d>"] = cmp.mapping.scroll_docs(-4),
 
           -- completion
-          ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-          ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+          ["<c-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+          ["<c-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
           ["<c-space>"] = cmp.mapping.complete(),
 
           -- confirmation
@@ -81,10 +96,15 @@ return {
             cmp.ItemField.Abbr,
             cmp.ItemField.Menu,
           },
-          format = function(_, item)
-            item.menu = " · " .. (item.kind or "Unknown")
-            item.kind = icons.completion.kinds[item.kind] or icons.misc.Question
+          format = function(entry, item)
+            local source = entry.source.name
+
+            if source and #source > 0 then
+              item.menu = " · " .. source_names[source] or source
+            end
+
             item.abbr = " " .. item.abbr
+            item.kind = icons.completion.kinds[item.kind] or icons.misc.Question
 
             return item
           end,
@@ -95,9 +115,8 @@ return {
           ghost_text = {
             enabled = true,
           },
-          native_menu = false,
         },
-      }, opts)
+      }
     end,
 
     config = function(_, opts)
@@ -105,25 +124,21 @@ return {
 
       cmp.setup(opts)
 
-      cmp.setup.cmdline('/', {
+      cmp.setup.cmdline("/", {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
-        }
+        sources = { { name = "buffer" } }
       })
 
-      cmp.setup.cmdline(':', {
+      cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
+        sources = {
+          { name = "path", group_index = 1 },
           {
-            name = 'cmdline',
-            option = {
-              ignore_cmds = { 'Man', '!' }
-            }
-          }
-        })
+            name = "cmdline",
+            group_index = 2,
+            option = { ignore_cmds = { "Man", "!" } },
+          },
+        },
       })
 
       -- autopairs
--- lua/plugins/editor/luasnip.lua
+++ lua/plugins/editor/luasnip.lua
@@ -1,67 +1,59 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
+  "L3MON4D3/LuaSnip",
 
-    build = "make install_jsregexp",
-    dependencies = { "rafamadriz/friendly-snippets" },
-
-    event = "InsertEnter",
-
-    config = function()
-      local ls = require("luasnip")
-
-      -- configuration
-      ls.config.set_config {
-        keep_roots = true,
-        link_roots = true,
-        link_children = true,
-        exit_roots = false,
-      }
-
-      -- load snippets
-      -- require("luasnip.loaders.from_lua").lazy_load()
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      -- override expand function
-      vim.snippet.expand = ls.lsp_expand
-      vim.snippet.stop = ls.unlink_current
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.snippet.active = function(filter)
-        if not filter or filter.direction ~= 1 then
-          return ls.jumpable(filter.direction)
-        else
-          return ls.expand_or_jumpable()
-        end
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.snippet.jump = function(direction)
-        if direction == 1 and ls.expandable() then
-          return ls.expand_or_jump()
-        else
-          return ls.jumpable(direction) and ls.jump(direction)
-        end
-      end
-
-      -- key mappings
-      local function activate(direction)
-        return function()
-          return vim.snippet.active { direction = direction } and vim.snippet.jump(direction)
-        end
-      end
-
-      vim.keymap.set({ "i", "s" }, "<c-j>", activate(1), { silent = true })
-      vim.keymap.set({ "i", "s" }, "<c-k>", activate(-1), { silent = true })
-    end,
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
   },
 
-  {
-    "hrsh7th/nvim-cmp",
-    optional = true,
-    dependencies = { "saadparwaiz1/cmp_luasnip" },
+  build = "make install_jsregexp",
+  event = "InsertEnter",
 
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "luasnip", group_index = 2 })
-      return opts
-    end,
-  }
+  config = function()
+    local ls = require("luasnip")
+
+    -- configuration
+    ls.config.set_config {
+      keep_roots = true,
+      link_roots = true,
+      link_children = true,
+      exit_roots = false,
+    }
+
+    -- load snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
+
+    -- override expand function
+    vim.snippet.expand = ls.lsp_expand
+    vim.snippet.stop = ls.unlink_current
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    function vim.snippet.active(filter)
+      if not filter or filter.direction ~= 1 then
+        return ls.jumpable(filter.direction)
+      else
+        return ls.expand_or_jumpable()
+      end
+    end
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    function vim.snippet.jump(direction)
+      if direction == 1 and ls.expandable() then
+        return ls.expand_or_jump()
+      else
+        return ls.jumpable(direction) and ls.jump(direction)
+      end
+    end
+
+    -- key mappings
+    local function activate(direction)
+      return function()
+        return vim.snippet.active { direction = direction } and vim.snippet.jump(direction)
+      end
+    end
+
+    vim.keymap.set({ "i", "s" }, "<c-j>", activate(1), { silent = true })
+    vim.keymap.set({ "i", "s" }, "<c-k>", activate(-1), { silent = true })
+  end,
 }
