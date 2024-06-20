90af4b526aeb99277462ae7782047642efe1c68f
Ayman Al-Qadhi <alqd@protonmail.com>
2024-06-20 19:59:31 +0300

    Add `nvim-bqf` plugin

---
+++ lua/plugins/ui/bqf.lua
@@ -0,0 +1,10 @@
+return {
+  "kevinhwang91/nvim-bqf",
+  event = "VeryLazy",
+  opts = {
+    preview = {
+      border = Void.config.ui.border,
+      winblend = Void.config.ui.float_winblend
+    }
+  },
+}
