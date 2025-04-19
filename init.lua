6ac94736458ede5d769556b328384154efd35cf8
Ayman Al-Qadhi <alqd@protonmail.com>
2025-04-19 05:10:56 +0300

    Disable python provider

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- disable unneeded providers
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
