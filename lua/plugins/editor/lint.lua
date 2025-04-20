add46a2d16fe71d79d29d1f3cb6ca578b572ca00
Ayman Al-Qadhi <alqd@protonmail.com>
2025-04-21 02:55:19 +0300

    Allow dynamic root dir selection for linters

--- lua/plugins/editor/lint.lua
+++ lua/plugins/editor/lint.lua
@@ -8,13 +8,44 @@ return {
   config = function()
     local lint = require("lint")
 
-    lint.linters_by_ft = {
-      lua = { "selene" },
-      sql = { "sqruff" },
+    -- override args
+    lint.linters.sqlfluff.args = { "lint", "--format=json" }
+
+    local linters_by_ft = {
+      lua = {
+        {
+          "selene",
+          cwd = function(args)
+            return vim.fs.root(args.file, "selene.toml")
+          end,
+          require_cwd = false,
+        },
+      },
+      sql = {
+        {
+          "sqlfluff",
+          cwd = function(args)
+            return vim.fs.root(args.file, { ".sqlfluff" })
+          end,
+          require_cwd = true,
+        },
+      },
     }
 
-    void.event.on("BufWritePost", function()
-      lint.try_lint()
+    void.event.on({ "BufEnter", "BufWritePost" }, function(args)
+      local ft = vim.bo[args.buf].filetype
+      local linters = linters_by_ft[ft]
+
+      if not linters or #linters == 0 then
+        return
+      end
+
+      for _, linter in ipairs(linters) do
+        local cwd = linter.cwd and linter.cwd(args)
+        if cwd or not linter.require_cwd then
+          require("lint").try_lint(linter[1], { cwd = cwd })
+        end
+      end
     end)
   end,
 }
