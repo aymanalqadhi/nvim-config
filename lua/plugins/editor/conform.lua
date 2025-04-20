b20d733476868d5f2e282eaca2c90a152d25d38c
Ayman Al-Qadhi <alqd@protonmail.com>
2025-04-21 02:55:27 +0300

    Switch back to `sqlfluff`

--- lua/plugins/editor/conform.lua
+++ lua/plugins/editor/conform.lua
@@ -23,7 +23,7 @@ return {
       json = { "prettierd", "prettier" },
       lua = { "stylua" },
       python = { "isort", "black" },
-      sql = { "sqruff" },
+      sql = { "sqlfluff" },
       typescript = { "prettierd", "prettier" },
       yaml = { "prettierd", "prettier", "yamlfmt" },
     },
