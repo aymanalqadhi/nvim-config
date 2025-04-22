ecd351bb1bb199681c3ba5f281c2663c6737cf34
Ayman Al-Qadhi <alqd@protonmail.com>
2025-04-22 03:24:44 +0300

    Fix workspace reuse issue for `gopls`

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -1,14 +1,50 @@
+local function run_with_env(cb)
+  if vim.g.go_env then
+    cb(vim.g.go_env)
+    return
+  end
+
+  local cmd = { "go", "env", "GOROOT", "GOMODCACHE" }
+  local opt = { text = true, stderr = false }
+
+  vim.system(cmd, opt, function(out)
+    if out.code == 0 then
+      local vars = vim.split(out.stdout, "\n", {
+        plain = true,
+        trimempty = false,
+      })
+      assert(#vars >= 2, "output must contain two lines")
+      vim.g.go_env = {
+        root = vars[1],
+        mod_cache = vars[2],
+      }
+    end
+
+    vim.schedule(function()
+      cb(vim.g.go_env)
+    end)
+  end)
+end
+
 return {
   cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gosum", "gotmpl" },
 
-  filetypes = {
-    "go",
-    "gomod",
-    "gosum",
-    "gotmpl",
-    "gohtmltmpl",
-    "gotexttmpl",
-  },
+  root_dir = function(bufnr, set)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+
+    run_with_env(function(env)
+      if vim.startswith(fname, env.root) or vim.startswith(fname, env.mod_cache) then
+        local clients = vim.lsp.get_clients({ name = "gopls" })
+        if #clients > 0 then
+          set(clients[#clients].config.root_dir)
+          return
+        end
+      end
+
+      set(vim.fs.root(fname, { "go.mod", "go.work", ".git" }))
+    end)
+  end,
 
   capabilities = {
     textDocument = {
