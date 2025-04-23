ec1515adbcc34aa86d456695dd9f03ca2b607bf8
Ayman Al-Qadhi <alqd@protonmail.com>
2025-04-24 00:42:51 +0300

    Add `rustaceanvim` plugin for rust support

---
+++ lua/plugins/lang/rust.lua
@@ -0,0 +1,25 @@
+return {
+  "mrcjkb/rustaceanvim",
+
+  version = "^6", -- Recommended
+  lazy = false, -- This plugin is already lazy
+
+  init = function()
+    vim.g.rustaceanvim = {
+      -- Plugin configuration
+      tools = {},
+      -- LSP configuration
+      server = {
+        on_attach = function(client, bufnr)
+          -- you can also put keymaps in here
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {},
+        },
+      },
+      -- DAP configuration
+      dap = {},
+    }
+  end,
+}
