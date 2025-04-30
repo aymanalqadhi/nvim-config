d8c978b080957b5f9584cb7ae54e64a2dfc61cf4
Ayman Al-Qadhi <alqd@protonmail.com>
2025-04-30 05:54:54 +0300

    Add `crates.nvim` plugin

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -1,25 +1,64 @@
 return {
-  "mrcjkb/rustaceanvim",
+  {
+    "mrcjkb/rustaceanvim",
 
-  version = "^6", -- Recommended
-  lazy = false, -- This plugin is already lazy
+    version = "^6", -- Recommended
+    lazy = false, -- This plugin is already lazy
 
-  init = function()
-    vim.g.rustaceanvim = {
-      -- Plugin configuration
-      tools = {},
-      -- LSP configuration
-      server = {
-        on_attach = function(client, bufnr)
-          -- you can also put keymaps in here
-        end,
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {},
+    init = function()
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {},
+        -- LSP configuration
+        server = {
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ["rust-analyzer"] = {
+              experimental = {
+                serverStatusNotification = true,
+              },
+            },
+          },
+        },
+        -- DAP configuration
+        dap = {},
+      }
+    end,
+  },
+  {
+    "saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+
+    opts = {
+      completion = {
+        blink = {
+          use_custom_kind = true,
+          kind_text = {
+            version = "Version",
+            feature = "Feature",
+          },
+          kind_highlight = {
+            version = "BlinkCmpKindVersion",
+            feature = "BlinkCmpKindFeature",
+          },
+          kind_icon = {
+            version = " ",
+            feature = " ",
+          },
+        },
+        crates = {
+          enabled = true,
+          min_chars = 3,
+          max_results = 8,
         },
       },
-      -- DAP configuration
-      dap = {},
-    }
-  end,
+      lsp = {
+        enabled = true,
+        name = "crates.nvim",
+        actions = true,
+        completion = true,
+        hover = true,
+      },
+    },
+  },
 }
