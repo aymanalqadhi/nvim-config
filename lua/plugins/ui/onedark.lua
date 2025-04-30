e924498dcde446f50b368eb1d7f6a689dbfe6dd6
Ayman Al-Qadhi <alqd@protonmail.com>
2025-04-30 05:55:18 +0300

    Use upstream origin

--- lua/plugins/ui/onedark.lua
+++ lua/plugins/ui/onedark.lua
@@ -1,5 +1,5 @@
 return {
-  "aymanalqadhi/onedark.nvim",
+  "navarasu/onedark.nvim",
 
   lazy = false,
   priority = 1000,
@@ -15,12 +15,12 @@ return {
     local bg_title = "$cyan"
 
     require("onedark").setup({
-      style         = "dark",
-      transparent   = true,
-      term_colors   = true,
+      style = "dark",
+      transparent = true,
+      term_colors = true,
       ending_tildes = true,
 
-      code_style    = {
+      code_style = {
         comments = "italic",
         keywords = "bold",
         functions = "none",
@@ -28,74 +28,78 @@ return {
         variables = "none",
       },
 
-      lualine       = {
+      lualine = {
         transparent = false,
       },
 
-      highlights    = {
+      highlights = {
         -- bqf
-        BqfPreviewFloat                = { fg = fg, bg = bg_dark },
-        BqfPreviewTitle                = { fg = fg_title, bg = bg_title },
+        BqfPreviewFloat = { fg = fg, bg = bg_dark },
+        BqfPreviewTitle = { fg = fg_title, bg = bg_title },
 
         -- float
-        FloatNormal                    = { fg = fg, bg = bg_dark },
-        FloatBorder                    = { fg = fg_border, bg = bg_dark },
-        FloatTitle                     = { fg = fg_title, bg = bg_title },
+        FloatNormal = { fg = fg, bg = bg_dark },
+        FloatBorder = { fg = fg_border, bg = bg_dark },
+        FloatTitle = { fg = fg_title, bg = bg_title },
 
         -- folds
-        FoldColumn                     = { bg = "NONE" },
-        Folded                         = { bg = bg_dark },
+        FoldColumn = { bg = "NONE" },
+        Folded = { bg = bg_dark },
 
         -- lsp highlight
-        LspReferenceRead               = { bg = bg_highlight },
-        LspReferenceWrite              = { bg = bg_highlight },
-        LspReferenceText               = { bg = bg_highlight },
+        LspReferenceRead = { bg = bg_highlight },
+        LspReferenceWrite = { bg = bg_highlight },
+        LspReferenceText = { bg = bg_highlight },
 
         -- noice/mini
-        MsgArea                        = { fg = fg, bg = bg },
+        MsgArea = { fg = fg, bg = bg },
 
         -- pmenu
-        Pmenu                          = { fg = fg, bg = bg_highlight },
+        Pmenu = { fg = fg, bg = bg_highlight },
 
         -- Telescope
-        TelescopeNormal                = { fg = fg, bg = bg_dark },
-        TelescopeBorder                = { fg = fg_border, bg = bg_dark },
-        TelescopeTitle                 = { fg = fg_title, bg = bg_title },
-        TelescopePromptBorder          = { fg = fg_border, bg = bg_dark },
-        TelescopeResultsBorder         = { fg = fg_border, bg = bg_dark },
-        TelescopePreviewBorder         = { fg = fg_border, bg = bg_dark },
+        TelescopeNormal = { fg = fg, bg = bg_dark },
+        TelescopeBorder = { fg = fg_border, bg = bg_dark },
+        TelescopeTitle = { fg = fg_title, bg = bg_title },
+        TelescopePromptBorder = { fg = fg_border, bg = bg_dark },
+        TelescopeResultsBorder = { fg = fg_border, bg = bg_dark },
+        TelescopePreviewBorder = { fg = fg_border, bg = bg_dark },
 
         -- toggleterm
-        ToggleTermFloatNormal          = { fg = fg, bg = bg_dark },
-        ToggleTermFloatBorder          = { fg = fg_border, bg = bg_dark },
+        ToggleTermFloatNormal = { fg = fg, bg = bg_dark },
+        ToggleTermFloatBorder = { fg = fg_border, bg = bg_dark },
 
         -- treesitter context
-        TreesitterContextLineNumber    = { bg = bg },
+        TreesitterContextLineNumber = { bg = bg },
 
         -- noice
-        NoiceCmdlineIconCmdline        = { fg = bg_title },
+        NoiceCmdlineIconCmdline = { fg = bg_title },
         NoiceCmdlinePopupBorderCmdline = { fg = bg_title },
-        NoiceCmdlinePopupTitleCmdline  = { fg = fg_title, bg = bg_title },
+        NoiceCmdlinePopupTitleCmdline = { fg = fg_title, bg = bg_title },
 
-        NoiceCmdlineIconHelp           = { fg = "$blue" },
-        NoiceCmdlinePopupBorderHelp    = { fg = "$blue" },
-        NoiceCmdlinePopupTitleHelp     = { fg = fg_title, bg = "$blue" },
+        NoiceCmdlineIconHelp = { fg = "$blue" },
+        NoiceCmdlinePopupBorderHelp = { fg = "$blue" },
+        NoiceCmdlinePopupTitleHelp = { fg = fg_title, bg = "$blue" },
 
-        NoiceCmdlineIconLua            = { fg = "$purple" },
-        NoiceCmdlinePopupBorderLua     = { fg = "$purple" },
-        NoiceCmdlinePopupTitleLua      = { fg = fg_title, bg = "$purple" },
+        NoiceCmdlineIconLua = { fg = "$purple" },
+        NoiceCmdlinePopupBorderLua = { fg = "$purple" },
+        NoiceCmdlinePopupTitleLua = { fg = fg_title, bg = "$purple" },
 
-        NoiceCmdlinePrompt             = { fg = "$green" },
-        NoiceCmdlineIconInput          = { fg = "$green" },
-        NoiceCmdlinePopupTitleInput    = { fg = fg_title, bg = "$green" },
-        NoiceCmdlinePopupBorderInput   = { fg = "$green" },
+        NoiceCmdlinePrompt = { fg = "$green" },
+        NoiceCmdlineIconInput = { fg = "$green" },
+        NoiceCmdlinePopupTitleInput = { fg = fg_title, bg = "$green" },
+        NoiceCmdlinePopupBorderInput = { fg = "$green" },
 
         -- nvim-tree
-        NvimTreeNormal                 = { fg = fg, bg = bg_dark },
-        NvimTreeNormalFloat            = { fg = fg, bg = bg_dark },
+        NvimTreeNormal = { fg = fg, bg = bg_dark },
+        NvimTreeNormalFloat = { fg = fg, bg = bg_dark },
+
+        -- cmp/blink
+        CmpItemAbbrMatch = { fg = "none", fmt = "bold" },
+        BlinkCmpLabelMatch = { fg = "none", fmt = "bold" },
       },
 
-      diagnostics   = {
+      diagnostics = {
         darker = true,
         undercurl = true,
         background = true,
