50b7670f2e38a872166a1580545a90155572f58d
Ayman Al-Qadhi <alqd@protonmail.com>
2025-06-03 20:59:49 +0300

    Update `neocmakelsp` config

--- lsp/neocmakelsp.lua
+++ lsp/neocmakelsp.lua
@@ -1,5 +1,26 @@
 return {
   cmd = { "neocmakelsp", "--stdio" },
   filetypes = { "cmake" },
-  root_markers = { ".git", "build", "cmake" },
+  root_markers = { "build", "cmake", ".git" },
+  workspace_required = false,
+  capabilities = {
+    textDocument = {
+      completion = {
+        completionItem = {
+          snippetSupport = true,
+        },
+      },
+    },
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+        relative_pattern_support = true,
+      },
+    },
+  },
+  init_options = {
+    format = { enable = false },
+    lint = { enable = true },
+    scan_cmake_in_package = true,
+  },
 }
