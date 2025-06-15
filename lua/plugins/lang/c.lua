de325bc1dad320ce5394dbbb8c8ab12ef4f6b312
Ayman Al-Qadhi <alqd@protonmail.com>
2025-06-15 23:44:30 +0300

    Add `clangd_extensions` plugin

---
+++ lua/plugins/lang/c.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    config = function() end,
+    opts = {
+      inlay_hints = {
+        inline = false,
+      },
+      ast = {
+        role_icons = {
+          type = "",
+          declaration = "",
+          expression = "",
+          specifier = "",
+          statement = "",
+          ["template argument"] = "",
+        },
+        kind_icons = {
+          Compound = "",
+          Recovery = "",
+          TranslationUnit = "",
+          PackExpansion = "",
+          TemplateTypeParm = "",
+          TemplateTemplateParm = "",
+          TemplateParamObject = "",
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+
+    opts = {
+      ensure_install = { "c", "cpp", "make", "cmake" },
+    },
+  },
+}
