12ebbe897479c720c86c683d39374d38e97c47a1
Ayman Al-Qadhi <alqd@protonmail.com>
2025-06-15 23:45:03 +0300

    Use diagnostic icons for virtual text when possible

--- plugin/diagnostic.lua
+++ plugin/diagnostic.lua
@@ -5,15 +5,23 @@ vim.diagnostic.config({
   virtual_text = {
     spacing = 4,
     source = "if_many",
-    prefix = void.config.icons.diagnostics.prefix,
+    prefix = function(diagnostic)
+      local icons = void.config.icons.diagnostics
+      for d, icon in pairs(icons.signs) do
+        if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+          return icon
+        end
+      end
+      return icons.prefix
+    end,
   },
   severity_sort = true,
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = void.config.icons.diagnostics.signs.Error,
-      [vim.diagnostic.severity.WARN]  = void.config.icons.diagnostics.signs.Warning,
-      [vim.diagnostic.severity.INFO]  = void.config.icons.diagnostics.signs.Info,
-      [vim.diagnostic.severity.HINT]  = void.config.icons.diagnostics.signs.Hint,
+      [vim.diagnostic.severity.WARN] = void.config.icons.diagnostics.signs.Warning,
+      [vim.diagnostic.severity.INFO] = void.config.icons.diagnostics.signs.Info,
+      [vim.diagnostic.severity.HINT] = void.config.icons.diagnostics.signs.Hint,
     },
   },
 })
