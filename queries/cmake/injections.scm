eb5d6d668cd284be905da00732fc7187957cdd1a
Ayman Al-Qadhi <alqd@protonmail.com>
2025-07-19 01:47:13 +0300

    Add highlight injections for some CMake functions

--- queries/cmake/injections.scm
+++ queries/cmake/injections.scm
@@ -1,6 +1,6 @@
 ;; extends
 
-
+; check_source_compiles (language set via first argument)
 (normal_command
   (identifier) @_name (#eq? @_name "check_source_compiles")
   (argument_list
@@ -10,6 +10,26 @@
       (bracket_argument
         (bracket_argument_open)
         (bracket_argument_content) @injection.content
-        (bracket_argument_close)))
-    )
-)
+        (bracket_argument_close)))))
+
+; check_c_source_compiles
+(normal_command
+  (identifier) @_name (#eq? @_name "check_c_source_compiles")
+  (argument_list
+    (argument
+      (bracket_argument
+        (bracket_argument_open)
+        (bracket_argument_content) @injection.content
+        (bracket_argument_close))))
+  (#set! injection.language "c"))
+
+; check_cxx_source_compiles
+(normal_command
+  (identifier) @_name (#eq? @_name "check_cxx_source_compiles")
+  (argument_list
+    (argument
+      (bracket_argument
+        (bracket_argument_open)
+        (bracket_argument_content) @injection.content
+        (bracket_argument_close))))
+  (#set! injection.language "cpp"))
